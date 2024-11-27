b45c1a094930d5d4108dbce15b23f676a985cf6c
ashutosh sahu <ppaskumar@gmail.com>
2024-11-27 16:07:30 +0530

    shift mongodb online server

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Define the mongoDB connection URL
 
-const mongoURL = "mongodb://localhost:27017/mycustomdatabase"; // Replace mycustomdatabase with your database name
+// const mongoURL = "mongodb://localhost:27017/mycustomdatabase"; // Replace mycustomdatabase with your database name
+const mongoURL = process.env.MONGODB_URL;
 
 // setup mongoDB Connection
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
@@ -43,6 +45,6 @@ const menuRoutes = require("./routes/menuRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is listing to port 3000");
 });
